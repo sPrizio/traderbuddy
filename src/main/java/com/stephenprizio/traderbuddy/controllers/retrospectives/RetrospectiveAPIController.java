75171e155470fd56e1df014781bf6c512883c014
Stephen <s.prizio@hotmail.com>
2022-09-21 14:34:53 -0400

    implements tests

--- src/main/java/com/stephenprizio/traderbuddy/controllers/retrospectives/RetrospectiveAPIController.java
+++ src/main/java/com/stephenprizio/traderbuddy/controllers/retrospectives/RetrospectiveAPIController.java
@@ -1,22 +1,22 @@
 package com.stephenprizio.traderbuddy.controllers.retrospectives;
 
 import com.stephenprizio.traderbuddy.converters.retrospectives.RetrospectiveDTOConverter;
+import com.stephenprizio.traderbuddy.enums.AggregateInterval;
 import com.stephenprizio.traderbuddy.models.entities.retrospectives.Retrospective;
 import com.stephenprizio.traderbuddy.models.records.json.StandardJsonResponse;
 import com.stephenprizio.traderbuddy.services.retrospectives.RetrospectiveService;
+import org.apache.commons.lang3.EnumUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
-import static com.stephenprizio.traderbuddy.validation.GenericValidator.validateIfAnyResult;
-import static com.stephenprizio.traderbuddy.validation.GenericValidator.validateLocalDateFormat;
+import static com.stephenprizio.traderbuddy.validation.GenericValidator.*;
 
 /**
  * API Controller for {@link Retrospective}s
@@ -27,6 +27,10 @@ import static com.stephenprizio.traderbuddy.validation.GenericValidator.validate
 @RestController
 @RequestMapping("${base.api.controller.endpoint}/retrospectives")
 public class RetrospectiveAPIController {
+
+    private static final String START_DATE_INVALID_FORMAT = "The start date %s was not of the expected format %s";
+    private static final String END_DATE_INVALID_FORMAT = "The end date %s was not of the expected format %s";
+    private static final List<String> REQUIRED_JSON_VALUES = List.of("retrospective");
 
     private static final String DATE_FORMAT = "yyyy-MM-dd";
 
@@ -49,15 +53,90 @@ public class RetrospectiveAPIController {
      * @param end end date
      * @return {@link StandardJsonResponse}
      */
+    @ResponseBody
     @GetMapping("/timespan")
     public StandardJsonResponse getRetrospectivesForTimespan(final @RequestParam("start") String start, final @RequestParam("end") String end) {
 
-        validateLocalDateFormat(start, DATE_FORMAT, "The start date %s was not of the expected format %s", start, DATE_FORMAT);
-        validateLocalDateFormat(end, DATE_FORMAT, "The end date %s was not of the expected format %s", end, DATE_FORMAT);
+        validateLocalDateFormat(start, DATE_FORMAT, START_DATE_INVALID_FORMAT, start, DATE_FORMAT);
+        validateLocalDateFormat(end, DATE_FORMAT, END_DATE_INVALID_FORMAT, end, DATE_FORMAT);
 
         List<Retrospective> retrospectives = this.retrospectiveService.findAllRetrospectivesWithinDate(LocalDate.parse(start, DateTimeFormatter.ofPattern(DATE_FORMAT)), LocalDate.parse(end, DateTimeFormatter.ofPattern(DATE_FORMAT)));
         validateIfAnyResult(retrospectives, "No retrospectives were found within interval: [%s, %s]", start, end);
 
         return new StandardJsonResponse(true, this.retrospectiveDTOConverter.convertAll(retrospectives), StringUtils.EMPTY);
+    }
+
+    /**
+     * Obtains a retrospective for the given start date, end date and interval
+     *
+     * @param start start date
+     * @param end end date
+     * @param interval {@link AggregateInterval}
+     * @return {@link StandardJsonResponse}
+     */
+    @ResponseBody
+    @GetMapping("/unique")
+    public StandardJsonResponse getRetrospectiveForStartDateAndEndDateAndInterval(final @RequestParam("start") String start, final @RequestParam("end") String end, final @RequestParam("interval") String interval) {
+
+        validateLocalDateFormat(start, DATE_FORMAT, START_DATE_INVALID_FORMAT, start, DATE_FORMAT);
+        validateLocalDateFormat(end, DATE_FORMAT, END_DATE_INVALID_FORMAT, end, DATE_FORMAT);
+
+        if (!EnumUtils.isValidEnumIgnoreCase(AggregateInterval.class, interval)) {
+            return new StandardJsonResponse(false, null, String.format("%s was not a valid interval", interval));
+        }
+
+        Optional<Retrospective> retrospective = this.retrospectiveService.findRetrospectiveForStartDateAndEndDateAndInterval(LocalDate.parse(start, DateTimeFormatter.ISO_DATE), LocalDate.parse(end, DateTimeFormatter.ISO_DATE), AggregateInterval.valueOf(interval));
+        if (retrospective.isEmpty()) {
+            return new StandardJsonResponse(false, null, String.format("No retrospective was found for start date %s, end date %s and interval %s", start, end, interval));
+        }
+
+        return new StandardJsonResponse(true, retrospective.get(), StringUtils.EMPTY);
+    }
+
+
+    //  ----------------- POST REQUESTS -----------------
+
+    /**
+     * Creates a new {@link Retrospective}
+     *
+     * @param requestBody json request
+     * @return {@link StandardJsonResponse}
+     */
+    @ResponseBody
+    @PostMapping("/create")
+    public StandardJsonResponse postCreateRetrospective(final @RequestBody Map<String, Object> requestBody) {
+        validateJsonIntegrity(requestBody, REQUIRED_JSON_VALUES, "json did not contain of the required keys : %s", REQUIRED_JSON_VALUES.toString());
+        return new StandardJsonResponse(true, this.retrospectiveDTOConverter.convert(this.retrospectiveService.createRetrospective(requestBody)), StringUtils.EMPTY);
+    }
+
+
+    //  ----------------- PUT REQUESTS -----------------
+
+    /**
+     * Updates an existing {@link Retrospective}
+     *
+     * @param start start date
+     * @param end end date
+     * @param interval {@link AggregateInterval}
+     * @param requestBody json request
+     * @return {@link StandardJsonResponse}
+     */
+    @ResponseBody
+    @PutMapping("/update")
+    public StandardJsonResponse putUpdateRetrospective(final @RequestParam("start") String start, final @RequestParam("end") String end, final @RequestParam("interval") String interval, final @RequestBody Map<String, Object> requestBody) {
+
+        validateJsonIntegrity(requestBody, REQUIRED_JSON_VALUES, "json did not contain of the required keys : %s", REQUIRED_JSON_VALUES.toString());
+        validateLocalDateFormat(start, DATE_FORMAT, START_DATE_INVALID_FORMAT, start, DATE_FORMAT);
+        validateLocalDateFormat(end, DATE_FORMAT, END_DATE_INVALID_FORMAT, end, DATE_FORMAT);
+
+        if (!EnumUtils.isValidEnumIgnoreCase(AggregateInterval.class, interval)) {
+            return new StandardJsonResponse(false, null, String.format("%s was not a valid interval", interval));
+        }
+
+        return new StandardJsonResponse(
+                true,
+                this.retrospectiveDTOConverter.convert(this.retrospectiveService.updateRetrospective(LocalDate.parse(start, DateTimeFormatter.ISO_DATE), LocalDate.parse(end, DateTimeFormatter.ISO_DATE), AggregateInterval.valueOf(interval), requestBody)),
+                StringUtils.EMPTY
+        );
     }
 }
--- src/test/java/com/stephenprizio/traderbuddy/controllers/retrospectives/RetrospectiveAPIControllerTest.java
+++ src/test/java/com/stephenprizio/traderbuddy/controllers/retrospectives/RetrospectiveAPIControllerTest.java
@@ -1,6 +1,8 @@
 package com.stephenprizio.traderbuddy.controllers.retrospectives;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.stephenprizio.traderbuddy.AbstractGenericTest;
+import com.stephenprizio.traderbuddy.enums.AggregateInterval;
 import com.stephenprizio.traderbuddy.services.retrospectives.RetrospectiveService;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.util.LinkedMultiValueMap;
@@ -17,11 +20,13 @@ import org.springframework.util.MultiValueMap;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -45,6 +50,10 @@ public class RetrospectiveAPIControllerTest extends AbstractGenericTest {
     @Before
     public void setUp() {
         Mockito.when(this.retrospectiveService.findAllRetrospectivesWithinDate(any(), any())).thenReturn(generateRetrospectives());
+        Mockito.when(this.retrospectiveService.findRetrospectiveForStartDateAndEndDateAndInterval(LocalDate.of(2022, 9, 5), LocalDate.of(2022, 9, 6), AggregateInterval.MONTHLY)).thenReturn(Optional.empty());
+        Mockito.when(this.retrospectiveService.findRetrospectiveForStartDateAndEndDateAndInterval(LocalDate.of(2022, 9, 10), LocalDate.of(2022, 9, 15), AggregateInterval.MONTHLY)).thenReturn(Optional.of(generateRetrospectives().get(1)));
+        Mockito.when(this.retrospectiveService.createRetrospective(any())).thenReturn(generateRetrospectives().get(0));
+        Mockito.when(this.retrospectiveService.updateRetrospective(any(), any(), any(), any())).thenReturn(generateRetrospectives().get(1));
     }
 
 
@@ -83,6 +92,204 @@ public class RetrospectiveAPIControllerTest extends AbstractGenericTest {
 
         this.mockMvc.perform(get("/api/v1/retrospectives/timespan").params(map))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.data[0].startDate", is(LocalDate.of(2022, 9, 5))));
+                .andExpect(jsonPath("$.data[0].startDate", is("2022-09-05")));
+    }
+
+
+    //  ----------------- getRetrospectiveForStartDateAndEndDateAndInterval -----------------
+
+    @Test
+    public void test_getRetrospectiveForStartDateAndEndDateAndInterval_badRequest_start() throws Exception {
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.put("start", List.of("asdasdasdasd"));
+        map.put("end", List.of("2022-09-18"));
+        map.put("interval", List.of("MONTHLY"));
+
+        this.mockMvc.perform(get("/api/v1/retrospectives/unique").params(map))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message", containsString("The start date asdasdasdasd was not of the expected format yyyy-MM-dd")));
+    }
+
+    @Test
+    public void test_getRetrospectiveForStartDateAndEndDateAndInterval_badRequest_end() throws Exception {
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.put("start", List.of("2022-09-12"));
+        map.put("end", List.of("adfafdsfdsfsd"));
+        map.put("interval", List.of("MONTHLY"));
+
+        this.mockMvc.perform(get("/api/v1/retrospectives/unique").params(map))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message", containsString("The end date adfafdsfdsfsd was not of the expected format yyyy-MM-dd")));
+    }
+
+    @Test
+    public void test_getRetrospectiveForStartDateAndEndDateAndInterval_badRequest_interval() throws Exception {
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.put("start", List.of("2022-09-05"));
+        map.put("end", List.of("2022-09-18"));
+        map.put("interval", List.of("BAD"));
+
+        this.mockMvc.perform(get("/api/v1/retrospectives/unique").params(map))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message", containsString("BAD was not a valid interval")));
+    }
+
+    @Test
+    public void test_getRetrospectiveForStartDateAndEndDateAndInterval_noResult() throws Exception {
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.put("start", List.of("2022-09-05"));
+        map.put("end", List.of("2022-09-06"));
+        map.put("interval", List.of("MONTHLY"));
+
+        this.mockMvc.perform(get("/api/v1/retrospectives/unique").params(map))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message", containsString("No retrospective was found for start date 2022-09-05, end date 2022-09-06 and interval MONTHLY")));
+    }
+
+    @Test
+    public void test_getRetrospectiveForStartDateAndEndDateAndInterval_success() throws Exception {
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.put("start", List.of("2022-09-10"));
+        map.put("end", List.of("2022-09-15"));
+        map.put("interval", List.of("MONTHLY"));
+
+        this.mockMvc.perform(get("/api/v1/retrospectives/unique").params(map))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.startDate", is("2022-09-12")));
+    }
+
+
+    //  ----------------- postCreateRetrospective -----------------
+
+    @Test
+    public void test_postCreateRetrospective_badJsonIntegrity() throws Exception {
+        this.mockMvc.perform(post("/api/v1/retrospectives/create").contentType(MediaType.APPLICATION_JSON).content(new ObjectMapper().writeValueAsString(Map.of("hello", "world"))))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message", containsString("json did not contain of the required keys : [retrospective]")));
+    }
+
+    @Test
+    public void test_postCreateRetrospective_success() throws Exception {
+
+        Map<String, Object> data =
+                Map.of(
+                        "retrospective",
+                        Map.of(
+                                "startDate", "2022-09-05",
+                                "endDate", "2022-09-11",
+                                "interval", "MONTHLY",
+                                "points", List.of(
+                                        Map.of(
+                                                "lineNumber", 1,
+                                                "entryText", "Test 1",
+                                                "keyPoint", "true"
+                                        ),
+                                        Map.of(
+                                                "lineNumber", 2,
+                                                "entryText", "Test 2",
+                                                "keyPoint", "false"
+                                        )
+                                )
+                        )
+                );
+
+        this.mockMvc.perform(post("/api/v1/retrospectives/create").contentType(MediaType.APPLICATION_JSON).content(new ObjectMapper().writeValueAsString(data)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.startDate", is("2022-09-05")));
+    }
+
+
+    //  ----------------- putUpdateRetrospective -----------------
+
+    @Test
+    public void test_putUpdateRetrospective_badRequest_start() throws Exception {
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.put("start", List.of("asdasdasdasd"));
+        map.put("end", List.of("2022-09-18"));
+        map.put("interval", List.of("MONTHLY"));
+
+        this.mockMvc.perform(put("/api/v1/retrospectives/update").params(map).contentType(MediaType.APPLICATION_JSON).content(new ObjectMapper().writeValueAsString(Map.of("retrospective", "world"))))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message", containsString("The start date asdasdasdasd was not of the expected format yyyy-MM-dd")));
+    }
+
+    @Test
+    public void test_putUpdateRetrospective_badRequest_end() throws Exception {
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.put("start", List.of("2022-09-12"));
+        map.put("end", List.of("adfafdsfdsfsd"));
+        map.put("interval", List.of("MONTHLY"));
+
+        this.mockMvc.perform(put("/api/v1/retrospectives/update").params(map).contentType(MediaType.APPLICATION_JSON).content(new ObjectMapper().writeValueAsString(Map.of("retrospective", "world"))))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message", containsString("The end date adfafdsfdsfsd was not of the expected format yyyy-MM-dd")));
+    }
+
+    @Test
+    public void test_putUpdateRetrospective_badRequest_interval() throws Exception {
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.put("start", List.of("2022-09-05"));
+        map.put("end", List.of("2022-09-18"));
+        map.put("interval", List.of("BAD"));
+
+        this.mockMvc.perform(put("/api/v1/retrospectives/update").params(map).contentType(MediaType.APPLICATION_JSON).content(new ObjectMapper().writeValueAsString(Map.of("retrospective", "world"))))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message", containsString("BAD was not a valid interval")));
+    }
+
+    @Test
+    public void test_putUpdateRetrospective_badJsonIntegrity() throws Exception {
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.put("start", List.of("2022-09-10"));
+        map.put("end", List.of("2022-09-15"));
+        map.put("interval", List.of("MONTHLY"));
+
+        this.mockMvc.perform(put("/api/v1/retrospectives/update").params(map).contentType(MediaType.APPLICATION_JSON).content(new ObjectMapper().writeValueAsString(Map.of("hello", "world"))))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message", containsString("json did not contain of the required keys : [retrospective]")));
+    }
+
+    @Test
+    public void test_putUpdateRetrospective_success() throws Exception {
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.put("start", List.of("2022-09-10"));
+        map.put("end", List.of("2022-09-15"));
+        map.put("interval", List.of("MONTHLY"));
+
+        Map<String, Object> data =
+                Map.of(
+                        "retrospective",
+                        Map.of(
+                                "startDate", "2022-09-05",
+                                "endDate", "2022-09-11",
+                                "interval", "MONTHLY",
+                                "points", List.of(
+                                        Map.of(
+                                                "lineNumber", 1,
+                                                "entryText", "Test 1",
+                                                "keyPoint", "true"
+                                        ),
+                                        Map.of(
+                                                "lineNumber", 2,
+                                                "entryText", "Test 2",
+                                                "keyPoint", "false"
+                                        )
+                                )
+                        )
+                );
+
+        this.mockMvc.perform(put("/api/v1/retrospectives/update").params(map).contentType(MediaType.APPLICATION_JSON).content(new ObjectMapper().writeValueAsString(data)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data.startDate", is("2022-09-12")));
     }
 }
